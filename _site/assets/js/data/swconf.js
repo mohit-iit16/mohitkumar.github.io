e83ff97fece6d813e11b8c9d553b6830d8a799d8
mohit-iit16 <mohit.iit16@gmail.com>
2025-06-12 01:19:54 +0530

    init theme

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1749670245',resources: [
+    cacheName: 'chirpy-1749671266',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
